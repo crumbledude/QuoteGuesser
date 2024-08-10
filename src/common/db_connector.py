19690fe034cd45631454a37ceaca6f0cca8caa14
wjcrumbleholme <64094813+wjcrumbleholme@users.noreply.github.com>
2024-08-10 15:51:47 +0100

    First update

--- src/common/db_connector.py
+++ src/common/db_connector.py
@@ -1,5 +1,29 @@
 import mysql.connector
 
-class db():
-    def __int__(self):
-        pass
+class Db:
+    def __init__(self, host, port, user, password, database):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+
+        self.mydb = mysql.connector.connect(
+            host = self.host,
+            port = self.port,
+            user = self.user,
+            password = self.password,
+            database = self.database
+        )
+
+        self.cursor = self.mydb.cursor()
+
+    def get_all_values(self, table):
+        sql_formula = f"SELECT * FROM {table}"
+        self.cursor.execute(sql_formula)
+        return self.cursor.fetchall()
+
+    def get_rand_value(self, table):
+        sql_formula = f"SELECT * FROM {table} ORDER BY RAND()"
+        self.cursor.execute((sql_formula))
+        return self.cursor.fetchone()
--- src/discord/main.py
+++ src/discord/main.py
@@ -1,7 +1,25 @@
 import discord
+from common.db_connector import Db
+import os
+from dotenv import find_dotenv, load_dotenv
+
+#Load environment variables
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+TEST_DB_IP = os.getenv("TEST_DB_IP")
+TEST_DB_USER = os.getenv("TEST_DB_USER")
+TEST_DB_PASS = os.getenv("TEST_DB_PASS")
+REAL_DB_IP = os.getenv("REAL_DB_IP")
+REAL_DB_USER = os.getenv("REAL_DB_USER")
+REAL_DB_PASS = os.getenv("REAL_DB_PASS")
 
 def main():
-    
+    #test_db = Db("141.147.85.118", "25576", "testuser", "123password", "testdb")
+    # print(test_db.get_all_values('students'))
+    # print(test_db.get_rand_value("students"))
+    actuall_db = Db(REAL_DB_IP, "3306", REAL_DB_USER, REAL_DB_PASS, "quotedb")
+    print(actuall_db.get_rand_value("quotetbl"))
 
 
 if __name__ == '__main__':
